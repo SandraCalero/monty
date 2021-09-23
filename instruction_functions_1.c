df632e627629487da026d5e12e226163cc7dda66
JuanSebastianGB <3393@holbertonschool.com>
2021-09-23 01:36:29 +0000

    UPDD Chaanging back dprintf by fprintf

--- instruction_functions_1.c
+++ instruction_functions_1.c
@@ -19,7 +19,7 @@ void execute_add(stack_t **stack, unsigned int line_number, char **line,
 		free_stack_t(*stack);
 
 		fclose(*monty_file);
-		dprintf(STDOUT_FILENO, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	n = (*stack)->n;
